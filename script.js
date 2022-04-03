57d186c5c24ab744d0e3b287b65043a2808af61d
orisalomon <orisalomon12@gmail.com>
2022-04-03 17:14:30 +0300

    fixed layout changes

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 "use strict";
 // sticky navigation
-const nav = document.getElementById("navbar");
-const aboutSection = document.getElementById("about");
-const initalCoords = aboutSection.getBoundingClientRect();
-window.addEventListener("scroll", function (e) {
-  if (this.window.scrollY > initalCoords.top) {
-    nav.classList.add("sticky");
-  } else {
-    nav.classList.remove("sticky");
-  }
-});
+// const nav = document.getElementById("navbar");
+// const headerSection = document.getElementById("header");
+// const initalCoords = headerSection.getBoundingClientRect();
+// window.addEventListener("scroll", function (e) {
+//   if (this.window.scrollY > initalCoords.bottom) {
+//     nav.classList.add("sticky");
+//   } else {
+//     nav.classList.remove("sticky");
+//   }
+// });
 
 // function handleForm(e) {
 //   e.preventDefault();
 //   console.log("button clicked!");
 // }
+
+const logo = document.getElementById("logo");
+window.addEventListener("resize", function (e) {
+  if (window.innerWidth < 1350) {
+    logo.classList.add("hide-logo");
+  } else {
+    logo.classList.remove("hide-logo");
+  }
+});
